14db228ee1a4414092796eab8a0bde7f4f77b7c1
Young Sun <youngsunpark21@gmail.com>
2019-01-14 19:35:16 +0900

    expire test -1

--- models/mapChat.js
+++ models/mapChat.js
@@ -5,7 +5,7 @@ var mapChatSchema = new Schema({
   username: String,
   userID: Number,
   content: String,
-  timestamp: { type: Date, default: Date.now },
+  timestamp: { type: Date, default: Date.now, expires: 60 },
   latitude: Number,
   longitude: Number,
   chat_id: String
